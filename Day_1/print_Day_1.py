97b01ce1056ba5e25abf352f17cbcf1e2672898a
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-11 23:16:16 -0700

    Learn about the variables interchanging inbetween

--- Day_1/print_Day_1.py
+++ Day_1/print_Day_1.py
@@ -4,3 +4,13 @@ print("Venkat" + " " + "Reddy")
 Name = input("What is your name ?\n")
 print("Hello Sir, Your name length is : " + str (len(Name)))
 
+# Variables
+a = input("a : ")
+b = input("b : ")
+
+c = a
+a = b
+b = c
+
+print("a = " + a)
+print("b = " + b)
