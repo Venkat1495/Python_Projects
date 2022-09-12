f9de0bd41e72e07a76b54035eef0f07c89754ecb
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-12 15:07:09 -0700

    Adding to ints which are intially strings so.

--- Day_2/DataType_Day_2.py
+++ Day_2/DataType_Day_2.py
@@ -15,3 +15,22 @@ print(3.14159)
 # Boolean
 
 print(True & False)
+
+# type check
+
+print(type(3.14159))
+
+# type convertions int to str
+
+print(type(str(11_23_450)))
+
+# Code Challenge
+
+two_digit_number = input("Type any two digit number : ")
+two_digit_number = list(two_digit_number)
+final_digit = 0
+for i in two_digit_number:
+    digit_x = int(i)
+    final_digit = final_digit + int(digit_x)
+
+print(final_digit)
