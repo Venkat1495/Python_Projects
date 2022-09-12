329e287dcde9f57046ce01b3713b517c31f3dc30
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-11 23:02:35 -0700

    Learned How to concate str and int and about the len function.

--- Day_1/print_Day_1.py
+++ Day_1/print_Day_1.py
@@ -2,4 +2,5 @@ print("Day 1 - Python Print Function\nThe function is declared like this:\nprint
 print("Venkat" + " " + "Reddy")
 
 Name = input("What is your name ?\n")
-print("Hello " + Name)
+print("Hello Sir, Your name length is : " + str (len(Name)))
+
