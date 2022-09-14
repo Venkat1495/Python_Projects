22d56f79ecb13f7f72f4e33f4f06115a64f405d8
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-13 19:25:25 -0700

    leap year checking code

---
+++ Day_3/LeapYear.py
@@ -0,0 +1,6 @@
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0 and year % 100 != 0 and year % 400 == 0 :
+    print(f"So year {year} is a leap year")
+else :
+    print(f"So year {year} is not a leap year")
