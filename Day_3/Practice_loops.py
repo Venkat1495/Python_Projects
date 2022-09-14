06d2c3f5bd636da9f87759f95f44181e8ee5dc5c
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-13 18:44:00 -0700

    Leared about the Nested If loops

--- Day_3/Practice_loops.py
+++ Day_3/Practice_loops.py
@@ -4,6 +4,14 @@ height = int(input("what is your height in cm?\n"))
 
 if height >= 120 :
     print("you can ride the rollercoaster! (:")
+    age = int(input("What is your age?\n"))
+    if age < 12:
+        print("you have to pay $5 for ticket")
+    elif (age >= 12 and age <=18) :
+        print("you have to pay $7 for ticket")
+    elif age > 18 :
+        print("you have to pay $12 for ticket")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
@@ -18,3 +26,6 @@ if number_remainder == 0 :
     print("this is an even number.")
 elif number_remainder == 1 :
     print("this is an odd number.")
+
+# Nested If loops
+
