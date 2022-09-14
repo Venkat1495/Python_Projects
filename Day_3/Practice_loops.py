a4f35ec8c6ca429ca958385e5d69704db7b2e315
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-13 18:58:07 -0700

    Coded BMI Cal with there weight status

--- Day_3/Practice_loops.py
+++ Day_3/Practice_loops.py
@@ -29,3 +29,23 @@ elif number_remainder == 1 :
 
 # Nested If loops
 
+# BMI Cal second stage
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+BMI = weight / (height ** 2)
+BMI = round(BMI)
+
+     # in nested if no need to check for the and statement as bellow
+
+if BMI <= 18.5 :
+    print(f"you BMI is {BMI} and you are undeweight")
+elif BMI > 18.5 and BMI <= 25 :
+    print(f"you BMI is {BMI} and you are normal weight")
+elif BMI > 25 and BMI <= 30 :
+    print(f"you BMI is {BMI} and you are overweight")
+elif BMI > 30 and BMI <= 35 :
+    print(f"you BMI is {BMI} and you are obese")
+elif BMI > 35 :
+    print(f"you BMI is {BMI} and you are clinially obese")
