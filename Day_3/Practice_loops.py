80b8bba1815c1650c2454d50f6b14bc25d32847c
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-13 18:08:11 -0700

    Learnt about the IF loops in the python

---
+++ Day_3/Practice_loops.py
@@ -0,0 +1,13 @@
+# If loops
+print("Welcome to the rollercoaster!")
+height = int(input("what is your height in cm?\n"))
+
+if height >= 120 :
+    print("you can ride the rollercoaster! (:")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+
+# Weather number input is odd or even
+
+number = int(input(""))
