954eece730c60f6f782e97225f42bd4b0adfe3e3
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-13 19:40:49 -0700

    Changed the logic in the If condiftion to get correct leap year results

--- Day_3/LeapYear.py
+++ Day_3/LeapYear.py
@@ -1,6 +1,8 @@
 year = int(input("Which year do you want to check?"))
 
-if year % 4 == 0 and year % 100 != 0 and year % 400 == 0 :
+if year % 4 == 0 and year % 100 == 0 and year % 400 == 0 :
+    print(f"So year {year} is a leap year")
+elif year % 4 == 0 and year % 100 != 0 :
     print(f"So year {year} is a leap year")
 else :
     print(f"So year {year} is not a leap year")
