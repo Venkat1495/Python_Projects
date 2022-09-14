97a042bd9ad32e918a2fcc3803fc095ada0a1c26
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-13 20:01:41 -0700

    Created the Roller Coaster code with photos price

--- Day_3/LeapYear.py
+++ Day_3/LeapYear.py
@@ -6,3 +6,32 @@ elif year % 4 == 0 and year % 100 != 0 :
     print(f"So year {year} is a leap year")
 else :
     print(f"So year {year} is not a leap year")
+
+# Roller Coaster ride with pics
+
+print("Welcome to the rollercoaster!")
+height = int(input("what is your height in cm?\n"))
+price = 0
+if height >= 120 :
+    print("you can ride the rollercoaster! (:")
+    age = int(input("What is your age?\n"))
+    if age < 12:
+        price = 5
+        print(f"you have to pay ${price} for ticket")
+    elif (age >= 12 and age <=18) :
+        price = 7
+        print(f"you have to pay ${price} for ticket")
+    elif age > 18 :
+        price = 12
+        print(f"you have to pay ${price} for ticket")
+
+    photos = input(f"Do you want photos? Type 'yes' or 'no' : ")
+
+    if photos == "yes" :
+        price = price + 3
+        print(f"The total bill is ${price}")
+    else :
+        print(f"The total bill is ${price}")
+
+else:
+    print("Sorry, you have to grow taller before you can ride.")
