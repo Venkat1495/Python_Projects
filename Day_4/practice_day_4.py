d8523e153e7ad419f40bcc5b52782aa498858190
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-09-14 17:17:15 -0700

    Learnt about the how to get the random float number.

--- Day_4/practice_day_4.py
+++ Day_4/practice_day_4.py
@@ -1,3 +1,8 @@
 import random
 random_int = random.randint(1,10)
 print(random_int)
+
+random_float = random.random()
+print(random_float)
+
+print(random_float * 5) # So we will get the random float numbers from 0 to 4.9999 in between based on the number we mutiply
