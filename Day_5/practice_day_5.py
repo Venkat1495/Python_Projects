823a8551e8702309ac4fbbef4ae03b56009e61a4
Venkat Jawahar Reddy Yerrabathini <JawaharReddy@csu.fullerton.edu>
2022-09-18 01:52:45 -0700

    completed logic for the finding the Average Height using the for loop

--- Day_5/practice_day_5.py
+++ Day_5/practice_day_5.py
@@ -4,8 +4,20 @@ def forlearn() :
     for i in fruit :
         print(i)
 
+def average_height() :
+    student_h = input("Input a list of student heights ").split()
+    i = 0
+    sum = 0
+    for n in student_h : # you can give name for n in singular form 
+        student_h[i] = int(n)
+        i += 1
+        sum += int(n)
+
+    average = round(sum / i)
+    print(average)
 
 
 
 # My function calling
-forlearn()
+# forlearn()
+average_height()
