68dfa74bfe4a5e907621efc73317e7817832af57
Venkat Jawahar Reddy Yerrabathini <JawaharReddy@csu.fullerton.edu>
2022-09-17 23:56:51 -0700

    Completed the logic for the Rock Paper Scissors using the If statement

--- Day_4/RockScissorsPapers.py
+++ Day_4/RockScissorsPapers.py
@@ -1,4 +1,7 @@
+import random
+
 rock = '''
+Rock
     _______
 ---'   ____)
       (_____)
@@ -8,6 +11,7 @@ rock = '''
 '''
 
 paper = '''
+Paper
     _______
 ---'   ____)____
           ______)
@@ -17,6 +21,7 @@ paper = '''
 '''
 
 scissors = '''
+Scissors
     _______
 ---'   ____)____
           ______)
@@ -25,3 +30,27 @@ scissors = '''
 ---.__(___)
 '''
 
+RPS = [rock, paper, scissors]
+
+our_choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors : "))
+
+print(RPS[our_choose])
+
+cmp_choose = random.randint(0,2)
+
+print(f"Computer Chose : \n \n {RPS[cmp_choose]}")
+
+if our_choose == 0 and cmp_choose == 2 :
+    print("You Win!")
+elif our_choose == 2 and cmp_choose == 1 :
+    print("You Win!")
+elif our_choose == 1 and cmp_choose == 0 :
+    print("You Win!")
+elif our_choose == 2 and cmp_choose == 0 :
+    print("You Lose!")
+elif our_choose == 1 and cmp_choose == 2 :
+    print("You Lose!")
+elif our_choose == 0 and cmp_choose == 1 :
+    print("You Lose!")
+elif our_choose == cmp_choose :
+    print("It's a draw")
