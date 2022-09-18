45c132924941b1776b4bb0e96fbd59c2838772ad
Venkat Jawahar Reddy Yerrabathini <JawaharReddy@csu.fullerton.edu>
2022-09-18 01:04:47 -0700

    Kept the for loop learing inside the fuction

--- Day_5/practice_day_5.py
+++ Day_5/practice_day_5.py
@@ -1,6 +1,11 @@
 # For loop with using list
+def forlearn() :
+    fruit = ["apple", "mango", "orange"]
+    for i in fruit :
+        print(i)
 
-fruit = ["apple", "mango", "orange"]
 
-for i in fruit :
-    print(i)
+
+
+# My function calling
+forlearn()
