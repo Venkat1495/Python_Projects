024b8aa04a8e975756e43af375c8855d5ce0add1
Venkat Jawahar Reddy Yerrabathini <JawaharReddy@csu.fullerton.edu>
2022-09-28 14:40:51 -0700

    Parcticing nested lists and dictionary

---
+++ Day_9/nested_list_dict.py
@@ -0,0 +1,8 @@
+nested_lD = {
+    "city": ["Delhi", "Bangalore", "Fullerton", "Chennai"]
+    "movies":{
+        "Ram": "Krishna",
+        "Allu": "Yavadu",
+        "NTR": "RRR"
+              }
+}
