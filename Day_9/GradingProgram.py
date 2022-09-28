32109fddd3298b7d77e95e6a624de3f2000fd1f0
Venkat Jawahar Reddy Yerrabathini <JawaharReddy@csu.fullerton.edu>
2022-09-28 14:26:52 -0700

    writen logic for the grade program using the dictionary and for loop

--- Day_9/GradingProgram.py
+++ Day_9/GradingProgram.py
@@ -8,7 +8,17 @@ student_scores = {
 # 🚨 Don't change the code above 👆
 
 # TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
 
+for socres in student_scores:
+    if student_scores[socres] > 90 and student_scores[socres] <= 100:
+        student_grades[socres] = "Outstanding"
+    elif student_scores[socres] > 80 and student_scores[socres] <= 90:
+        student_grades[socres] = "Exceeds Expectations"
+    elif student_scores[socres] > 70 and student_scores[socres] <= 80:
+        student_grades[socres] = "Acceptable"
+    elif student_scores[socres] <= 70:
+        student_grades[socres] = "Fail"
 
 # TODO-2: Write your code below to add the grades to student_grades.👇
 
