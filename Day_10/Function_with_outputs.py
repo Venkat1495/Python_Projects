73434464116e9f24c1fcfb901ff0254462e0f6ef
Venkat Jawahar Reddy Yerrabathini <JawaharReddy@csu.fullerton.edu>
2022-10-03 15:05:01 -0700

    Checking pycharm commit and push

--- Day_10/Function_with_outputs.py
+++ Day_10/Function_with_outputs.py
@@ -5,7 +5,7 @@ def format_name(f_name, l_name):
     return f"{f_name} {l_name}"
 f_name = input("Please type your first name: ")
 l_name = input("Please type your last name: ")
-full_name = format_name(f_name, l_name)
+full_name = format_name(f_name,l_name)
 print(full_name)
 
 def is_leap(year):
