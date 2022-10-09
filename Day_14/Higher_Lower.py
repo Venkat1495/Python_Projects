edb98cc1ed368fa3f6066047b7f4a8d8aa1faf63
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-10-08 19:37:11 -0700

    Started the Day 14 Final project Higher and Lower

---
+++ Day_14/Higher_Lower.py
@@ -0,0 +1,13 @@
+# To-Do
+# Display Logo
+# Display 2 print statements
+# Comparing the 2 Variables
+# Adding the score for correct ans
+# displaying the final score
+
+# Imports
+from art import logo
+from game_data import data
+
+# Global Variables
+score = 0
