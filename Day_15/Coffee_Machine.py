f3c4839ddcce6df645ccd443bd572d2b33c43918
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-10-09 22:04:09 -0700

    Added the logic for the Coffee Machine and yet to do final changes

--- Day_15/Coffee_Machine.py
+++ Day_15/Coffee_Machine.py
@@ -30,10 +30,77 @@ resources = {
     "coffee": 100,
 }
 
-# TODO 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):"
-# TODO 2. Turn off the Coffee Machine by entering “​off”​to the prompt.
-# TODO 3. Print report.
-# TODO 4. Check resources sufficient?
-# TODO 5. Process coins.
-# TODO 6. Check transaction successful?
-# TODO 7. Make Coffee.
+
+def report_off(rf):
+    """Generate the report of current resources and also able to OFF the machine"""
+    if rf == "report":
+        print(f"Water: {resources['water']} \nMilk: {resources['milk']} \nCoffee: {resources['coffee']} \nMoney: {resources['money']}")
+        return True
+    elif rf == "off":
+        return False
+
+
+def re_check(user_bew):
+    menu_choosie = MENU[user_bew]["ingredients"]
+    if resources["water"] > menu_choosie["water"] :
+        if resources["milk"] > menu_choosie["milk"] :
+            if resources["coffee"] > menu_choosie["coffee"]:
+                return True
+            else:
+                print("Sorry there is not enough coffee")
+                return False
+        else:
+            print("Sorry there is not enough Milk")
+            return False
+    else:
+        print("Sorry there is not enough Water")
+        return False
+
+def update_res(user_req):
+    menu_choosie = MENU[user_req]["ingredients"]
+    resources["water"] -= menu_choosie["water"]
+    resources["milk"] -= menu_choosie["milk"]
+    resources["coffee"] -= menu_choosie["coffee"]
+    resources["money"] += MENU[user_req]["cost"]
+
+
+def coins(user_req):
+    menu_choosie = MENU[user_req]
+    qua = int(input("how many quarters?: "))
+    dim = int(input("how many dimes?: "))
+    nic = int(input("how many nickles?: "))
+    pen = int(input("how many pennies?: "))
+    total = (qua * 0.25) + (dim * 0.10) + (nic * 0.05) + (pen * 0.05)
+    if total >= menu_choosie["cost"]:
+        balance = total - menu_choosie["cost"]
+        print(f"Here is {balance} in change.")
+        update_res(user_req)
+        print(f"Here is your {user_req} ☕️. Enjoy!")
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+while True:
+    # TODO 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):"
+    user_req = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    # TODO 2. Turn off the Coffee Machine by entering "off”to the prompt.
+    # TODO 3. Print report.
+    resources["money"] = 0
+    is_true = True
+    if user_req == "report" or user_req == "off":
+        is_true = report_off(user_req)
+    if not is_true:
+        break
+    # TODO 4. Check resources sufficient?
+    if user_req == "espresso" or user_req == "latte" or user_req == "cappuccino" :
+        is_true = re_check(user_req)
+        if not is_true:
+            break
+        is_true = coins(user_req)
+        if not is_true:
+            break
+    # TODO 5. Process coins.
+
+    # TODO 6. Check transaction successful?
+    # TODO 7. Make Coffee.
