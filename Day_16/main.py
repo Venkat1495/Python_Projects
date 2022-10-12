c67ff8ef1f87139b29d68401469ea19ef79ee947
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-10-12 15:17:32 -0700

    Added Day 16 OOPS new coffee machine challenge start code

--- Day_16/main.py
+++ Day_16/main.py
@@ -1,11 +1,18 @@
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("DarkGreen")
-timmy.forward(100.00)
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkGreen")
+# timmy.forward(100.00)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
+
